72350a59eea1945540710bc493bfd0753f947d91
Milkey Tan <819774989@qq.com>
2018-05-14 09:50:36 +0800

    Update start.py

--- ErikoAPI/start.py
+++ ErikoAPI/start.py
@@ -2,7 +2,7 @@ from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from app import app
-print('Eriko API Start')
+print('Eriko API Running')
 http_server = HTTPServer(WSGIContainer(app))
 http_server.listen(5555)
 IOLoop.instance().start()
